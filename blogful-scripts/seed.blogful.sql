71c03041860a8ce5fd195b43d83e5aaff04b00f6
Cody Gillette <cgillette1312@gmail.com>
2019-05-07 14:18:39 -0700

    second-drills

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,24 @@
+
+
+INSERT INTO blogful_articles (title,date_published,content)
+VALUES
+    ('guileless inker',now() - '1 days'::INTERVAL,'Ink has no glue'),
+    ('meretricious foreman',now() - '1 days'::INTERVAL,'Nothing is finished'),
+    ('limpid animator',now() - '1 days'::INTERVAL,'droopy things'),
+    ('heuristic carousel',now() - '1 days'::INTERVAL,'we go round and round'),
+    ('loquacious exorcism',now() - '1 days'::INTERVAL,'you will be freed'),
+    ('uxorious glitter',now() - '9 days'::INTERVAL,'we make things shine '),
+    ('pendulous make-up artist',now() - '9 days'::INTERVAL,'who do you wish to look like'),
+    ('mannered set decorator',now() - '9 days'::INTERVAL,'Better use the right fork'),
+    ('propitious dragons',now() - '9 days'::INTERVAL,'THEY ARE REAL'),
+    ('withering potato',now() - '9 days'::INTERVAL,'Withing between become a chip or not '),
+    ('defamatory salad',now() - '7 days'::INTERVAL,'Its just a salad'),
+    ('egregious bobbin boy',now() - '7 days'::INTERVAL,'He will never stop'),
+    ('baleful laser',now() - '5 days'::INTERVAL,'Ill soon be a jedi '),
+    ('noxious nibblets',now() - '5 days'::INTERVAL,'nibblets that are noxious'),
+    ('salubrious stick figures',now() - '5 days'::INTERVAL,'Its about to go down'),
+    ('arcadian opera singer',now() - '5 days'::INTERVAL,'Beautiful, enough said'),
+    ('incendiary mechanic',now() - '5 days'::INTERVAL,'YOU GO FAST'),
+    ('concomitant carrot', now() - '5 days'::INTERVAL,'HMMMM'),
+    ('wheedling ice cream', now() - '5 days'::INTERVAL,'5 second fighting'),
+    ('precipitate unicorn', now() - '5 days'::INTERVAL,'winner');
