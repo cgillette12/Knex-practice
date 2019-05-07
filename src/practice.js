88f81f8c86f64c1d582f91066bc3b326255f9092
Cody Gillette <cgillette1312@gmail.com>
2019-05-07 12:22:19 -0700

    finished notes

--- src/practice.js
+++ src/practice.js
@@ -20,16 +20,49 @@ console.log('connetion successful');
 // }
 // search('holo');
 
-function paginateProducts(page){
-  const productsPrePage = 10;
-  const offset = productsPrePage * (page - 1);
-  knexInstance('amazong_products')
-    .select('product_id', 'name', 'price', 'category')
-    .limit(productsPrePage)
-    .offset(offset)
-    .then(results => {
-      console.log(results);
+// function paginateProducts(page){
+//   const productsPrePage = 10;
+//   const offset = productsPrePage * (page - 1);
+//   knexInstance('amazong_products')
+//     .select('product_id', 'name', 'price', 'category')
+//     .limit(productsPrePage)
+//     .offset(offset)
+//     .then(results => {
+//       console.log(results);
+//     });
+// }
+
+// paginateProducts(2);
+
+// function getProductsWithImages() {
+//   knexInstance
+//     .select('product_id', 'name', 'price', 'category', 'image')
+//     .from('amazong_products')
+//     .whereNotNull('image')
+//     .then(result => {
+//       console.log(result);
+//     });
+// }
+
+// getProductsWithImages();
+
+function mostPopularVideosForDays(days) {
+  knexInstance('whopipe_video_views')
+    .select('video_name', 'region')
+    .count('date_viewed AS views')
+    .where(
+      'date_viewed',
+      '>',
+      knexInstance.raw('now() -\' ?? days\':: INTERVAL', days)
+    )
+    .groupBy('video_name','region')
+    .orderBy([
+      {column:'region',order:'ASC'},
+      {column:'views',order:'DESC'}
+    ])
+    .then(result => {
+      console.log(result);
     });
 }
 
-paginateProducts(2);
+mostPopularVideosForDays(30);
