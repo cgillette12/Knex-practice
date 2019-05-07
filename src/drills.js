fa774d635c597b2f9fde5866b6bfe6c79195ca3d
Cody Gillette <cgillette1312@gmail.com>
2019-05-07 13:11:06 -0700

    dills finished

--- src/drills.js
+++ src/drills.js
@@ -17,3 +17,43 @@ function searchItemsbyName(searchTearm){
     });
 }
 searchItemsbyName('Fish');
+
+function paginated(pageNumber){
+  const productsPrePage = 6;
+  const offset = productsPrePage *(pageNumber-1);
+  knexInstance('shopping_list')
+    .select('id','name','price')
+    .limit(productsPrePage)
+    .offset(offset)
+    .then(results => {
+      console.log(results);
+    });
+}
+paginated(4);
+
+
+function sumItemCategories() {
+  knexInstance('shopping_list')
+    .select('category')
+    .count('name AS items')
+    .sum('price AS total')
+    .select(knexInstance.raw('ROUND(AVG(price), 2) AS average'))
+    .groupBy('category')
+    .then(result=>{
+      console.log(result);
+    });
+} 
+
+sumItemCategories();
+
+function costPerCategory() {
+  knexInstance('shopping_list')
+    .select('category')
+    .sum('price as total')
+    .groupBy('category')
+    .then(result => {
+      console.log(result);
+    });
+}
+
+costPerCategory();
